2568fbd2897ecbf38793dbf588c4f1d013992a16
Xuanwo <vars.cn@gmail.com>
2014-03-25 14:59:44 +0800

    明白了，那个clrscr就是逗我玩的= =

--- C语言等级考试模拟-第17套-1.cpp
+++ C语言等级考试模拟-第17套-1.cpp
@@ -1,3 +1,4 @@
+/*
 #include  <stdio.h>
 #include  <string.h>
 #include  <conio.h>
@@ -34,4 +35,36 @@ int main()
      fun(str1,str2);
      printf("******* The new string *******\n");
      puts (str1);
+}
+**/
+
+#include <stdio.h>
+#include <conio.h>
+#define N 40
+void fun(char *str1,char *str2)
+{
+	int i=0;
+	char *p1=str1;
+	char *p2=str2;
+	while(*(p1+i) )
+		i++;
+	for(  ;*p2; i++)
+		*(p1+i)= *p2++;
+	*(p1+i)='\0';
+}
+int main()
+{
+	char str1[N],str2[N];
+	int m,n,k;
+	printf("******* Input the  string str1 & str2*******\n ");
+	printf(" \nstr1:");
+	gets (str1);
+	printf(" \nstr2:");
+	gets (str2);
+	printf("******* The  string str1 & str2*******\n");
+	puts (str1);
+	puts (str2);
+	fun(str1,str2);
+	printf("******* The new string *******\n");
+	puts (str1);
 }
