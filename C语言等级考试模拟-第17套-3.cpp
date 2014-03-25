d3655c2c9d5a98226dde47bb8b88a0a00f014615
Xuanwo <vars.cn@gmail.com>
2014-03-25 15:09:57 +0800

    丧心病狂= =

--- C语言等级考试模拟-第17套-3.cpp
+++ C语言等级考试模拟-第17套-3.cpp
@@ -19,4 +19,28 @@ int  main()
 }
 **/
 
+#include <math.h>
+#include <stdio.h>
+double  fun(int  n)
+{
 
+	int i;
+	double s=1,p=1;
+	for (i=2;i<=n;i++)
+	{
+		p+=pow(i,0.5);
+		s+=p;
+	}
+	return s;
+
+}
+
+int main()
+{
+	int  n;
+	double  s;
+	printf("\n\nIput n:  ");
+	scanf("%d",&n);
+	s=fun(n);
+	printf("\n\ns=%f\n\n",s);
+}
