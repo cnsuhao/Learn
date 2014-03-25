bbdfc1a1129189375a2b5e6b9d5e8a77e89b25b1
Xuanwo <vars.cn@gmail.com>
2014-03-25 14:47:21 +0800

    蛋疼的题目= =

---
+++ C语言等级考试模拟-第17套-1.cpp
@@ -0,0 +1,34 @@
+#include  <stdio.h>
+#include  <string.h>
+
+#define N 40
+
+void fun(char *str1,char str2)
+{
+    int i;
+    char *p1=str1;
+    char *p2=str2;
+    while()
+        i++;
+    for( ; ; i++)
+        *(p1+i)= ;
+        *(p1+i)='\0';
+}
+
+int main()
+{
+     char str1[N],str2[N];
+     int m,n,k;
+     clrscr() ;
+     printf("******* Input the  string str1 & str2*******\n ");
+     printf(" \nstr1:");
+     gets (str1);
+     printf(" \nstr2:");
+     gets (str2);
+     printf("******* The  string str1 & str2*******\n");
+     puts (str1);
+     puts (str2);
+     fun(str1,str2);
+     printf("******* The new string *******\n");
+     puts (str1);
+}
