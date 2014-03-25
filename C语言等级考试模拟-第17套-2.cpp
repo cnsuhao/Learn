1f004feb433c049258a6cd9b5db6a57e7f67538e
Xuanwo <vars.cn@gmail.com>
2014-03-25 15:03:49 +0800

    头已晕

---
+++ C语言等级考试模拟-第17套-2.cpp
@@ -0,0 +1,52 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef  struct  aa
+{
+	int  data;
+	struct  aa  *next;
+}NODE;
+
+NODE  *Creatlink(int n,int m)
+{
+	NODE  *h=NULL,*p,*s;
+	int i;
+	s=(NODE*)malloc(sizeof(NODE));
+	/******************found****************/
+	h=p;
+	p->next=NULL;
+	for(i=1;i<n;i++)
+	{
+		s=(NODE*)malloc(sizeof(NODE));
+		s->data=rand()%m;
+		s->next=p->next;
+		p->next=s;
+		p=p->next;
+	}
+	/******************found****************/
+	return  p;
+}
+
+
+void outlink(NODE  *h)
+{
+	NODE *p;
+	p = h->next;
+	printf("\n\nTHE  LIST :\n\n  HEAD " );
+	while(p)
+	{
+		printf("->%d ",p->data ); p=p->next;
+	}
+	printf ("\n");
+}
+
+
+int  main()
+{
+	NODE *head;
+
+	head=Creatlink(8,22);
+	outlink(head);
+	return 0;
+}
