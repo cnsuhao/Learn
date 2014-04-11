43000e5de6b03edd6a148413a2833d119c38ac1d
Xuanwo <vars.cn@gmail.com>
2014-04-11 18:42:21 +0800

    = =
    加油

--- teset.cpp
+++ teset.cpp
@@ -1,9 +1,17 @@
-#include  <stdio.h>
+#include<stdio.h>
+ int main()
 
-int main()
 {
-    int a;
-    while(scanf("%d", &a)&&a!=0)
-    printf("%d\n", a);
-    return 0;
+    int x,y,z=0;
+    for(x=0;x<=100;x++)
+    {
+        for(y=0;y<=100;y++)
+        {
+            for(z=0;z<=50;z++)
+            {
+                if(3*x+2*y+z==100&&x+y+2*z==100)
+                    printf("x=%d,y=%d,z=%d\n",x,y,z);
+            }
+        }
+    }
 }
