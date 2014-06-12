973ce2dcf7e80acae165506f90c3d1e4fda63a55
Xuanwo <vars.cn@gmail.com>
2014-06-12 13:55:00 +0800

    test

--- test.c
+++ test.c
@@ -1,28 +1,7 @@
 #include  <stdio.h>
 
-
-strcmp(char  ___(9)_____, char *p2)
-/*两个字符串比较的函数*/
+main()
 {
-    int i；
-    ____(10)______；
-    while (*(pl + i) == *(p2 + i))
-        if (*(p1 + i++) == '\0')
-            return (0)；
-                   return (*(pl + i) - * (p2 + i)); /*不等时返回结果为第一个不等字符ASCII码的差值*/
-}
-
-
-int main()
-{
-    int m；
-    char  ___(6)____，str2[20]，*p1, *p2；
-    printf("Input two string：\n")；
-    scanf("%s", strl)；
-    scanf("%s"，___(7)______);
-    p1 = &strl[0];
-    p2 = _____(8)_____;
-    m = strcmp(p1, p2)；
-        printf("result:%d,\n", m);
-    return 0;
+    int i = 10, j = 10;
+    printf("%d, %d\n", i, j--);
 }
