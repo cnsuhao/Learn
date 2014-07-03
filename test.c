588d86b438a20f0bb47380bc478ed715ef980a81
Xuanwo <vars.cn@gmail.com>
2014-07-03 08:40:49 +0800

    不知道帮谁做的，已经不重要了= =

--- test.c
+++ test.c
@@ -1,7 +1,32 @@
-#include  <stdio.h>
-
-main()
+#include<iostream>
+using namespace std;
+int main()
 {
-    int i = 10, j = 10;
-    printf("%d, %d\n", i, j--);
+    int N,K,i,j;
+    double temp;
+    cin>>N>>K;
+    double *p=new double[N];
+    for(i=0;i<N;i++)
+    {
+        cin>>p[i];
+    }
+    for(i=0;i<N;i++)
+    {
+        for(j=N-1;j>1;j++)
+        {
+            if(p[j-1]<p[j])
+            {
+                temp=p[j-1];
+                p[j-1]=p[j];
+                p[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<N;i++)
+    {
+        if(i==0 ||i==K-1)
+            cout<<p[i];
+    }
+    delete p;
+    return 0;
 }
