98ab06606d80bb7d3d08efe992aa8d60ee3198a7
Xuanwo <vars.cn@gmail.com>
2014-08-15 23:33:52 +0800

    测试

--- test.c
+++ test.c
@@ -1,32 +1,37 @@
-#include<iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+#define pi (acos(-1.0))
+#define eps (1e-8)
+#define inf (1<<30)
 using namespace std;
-int main()
+
+
+int n,tmp;
+int main(int argc, char const *argv[])
 {
-    int N,K,i,j;
-    double temp;
-    cin>>N>>K;
-    double *p=new double[N];
-    for(i=0;i<N;i++)
+    while (1)
     {
-        cin>>p[i];
+        tmp=~scanf("%d", &n);
+        cout << tmp << endl;
     }
-    for(i=0;i<N;i++)
-    {
-        for(j=N-1;j>1;j++)
-        {
-            if(p[j-1]<p[j])
-            {
-                temp=p[j-1];
-                p[j-1]=p[j];
-                p[j]=temp;
-            }
-        }
-    }
-    for(i=0;i<N;i++)
-    {
-        if(i==0 ||i==K-1)
-            cout<<p[i];
-    }
-    delete p;
     return 0;
 }
