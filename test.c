82838fd06d3c5d3840b06208cbb612fecf376652
Xuanwo <vars.cn@gmail.com>
2014-09-02 18:13:22 +0800

    更新 测试

--- test.c
+++ test.c
@@ -23,17 +23,9 @@
 #define eps (1e-8)
 #define inf (1<<30)
 using namespace std;
-// numeric_limits example
-//#include <iostream>     // std::cout
-#include <limits>       // std::numeric_limits
 
 
-int main () {
-  std::cout << std::boolalpha;
-  std::cout << "Minimum value for int: " << std::numeric_limits<int>::min() << '\n';
-  std::cout << "Maximum value for int: " << std::numeric_limits<int>::max() << '\n';
-  std::cout << "int is signed: " << std::numeric_limits<int>::is_signed << '\n';
-  std::cout << "Non-sign bits in int: " << std::numeric_limits<int>::digits << '\n';
-  std::cout << "int has infinity: " << std::numeric_limits<int>::has_infinity << '\n';
-  return 0;
+main()
+{
+    printf("%s:%c%d", "pipi", 41, 83);
 }
